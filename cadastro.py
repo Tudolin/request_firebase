d3d9c9bc79227ca83029fefed67e6fbfe7fb6409
Rafael Nadalin <rafael.nadalin@outlook.com>
2023-08-31 16:28:45 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,10 +1,34 @@
 import requests
 import json
 
+#
+class AutoIncrementID:
+    def __init__(self):
+        self.load_last_id()
 
-create_userdata = {"id":"","name":"","lastname":"", "occupation":"" }
+    def load_last_id(self):
+        try:
+            with open("last_id.txt", "r") as file:
+                self.current_id = int(file.read().strip())
+        except FileNotFoundError:
+            self.current_id = 1
 
-def cadastro():
+    def save_last_id(self):
+        with open("last_id.txt", "w") as file:
+            file.write(str(self.current_id))
+
+    def generate_id(self):
+        new_id = self.current_id
+        self.current_id += 1
+        self.save_last_id()
+        return new_id
+    
+id_generator = AutoIncrementID()
+
+def cadastro(id_generator):
+
+    create_userdata = {"id": id_generator.generate_id(),"name":"","lastname":"", "occupation":"" }
+
     name = str(input("Insira seu nome = "))
     lastname = str(input("Insira seu sobrenome = "))
     occupation = str(input("Insira seu cargo = "))
@@ -12,12 +36,14 @@ def cadastro():
     create_userdata['lastname'] = lastname
     create_userdata['occupation'] = occupation
 
+    create_userdata_serial = json.dumps(create_userdata)
 
-cadastro()
+    request = requests.post(url='https://teste-estudos-4706a-default-rtdb.firebaseio.com/.json', data=create_userdata_serial)
+    print(request)
+    print(request.json())
 
-create_userdata_serial = json.dumps(create_userdata)
+cadastro(id_generator)
+
+lista = json.load()
 
 
-request = requests.post(url='https://teste-estudos-4706a-default-rtdb.firebaseio.com/.json', data=create_userdata_serial)
-print(request)
-print(request.json())
