91c359ad9515cf57e58ec9d7daabf94704629119
Rafael Nadalin <rafael.nadalin@outlook.com>
2023-09-01 16:02:16 -0300

    Atualização código
    Adicionando novas features e corrigindo alguns bugs no código

--- cadastro.py
+++ cadastro.py
@@ -1,7 +1,6 @@
 import requests
 import json
 
-#
 class AutoIncrementID:
     def __init__(self):
         self.load_last_id()
@@ -22,28 +21,64 @@ class AutoIncrementID:
         self.current_id += 1
         self.save_last_id()
         return new_id
-    
+
 id_generator = AutoIncrementID()
 
 def cadastro(id_generator):
+    while True:
+        user_id = id_generator.generate_id()
 
-    create_userdata = {"id": id_generator.generate_id(),"name":"","lastname":"", "occupation":"" }
+        create_userdata = {
+            "id": user_id,
+            "name": "",
+            "lastname": "",
+            "occupation": ""
+        }
 
-    name = str(input("Insira seu nome = "))
-    lastname = str(input("Insira seu sobrenome = "))
-    occupation = str(input("Insira seu cargo = "))
-    create_userdata['name'] = name
-    create_userdata['lastname'] = lastname
-    create_userdata['occupation'] = occupation
+        name = str(input("Insira seu nome = "))
+        lastname = str(input("Insira seu sobrenome = "))
+        occupation = str(input("Insira seu cargo = "))
 
-    create_userdata_serial = json.dumps(create_userdata)
+        create_userdata['name'] = name
+        create_userdata['lastname'] = lastname
+        create_userdata['occupation'] = occupation
 
-    request = requests.post(url='https://teste-estudos-4706a-default-rtdb.firebaseio.com/.json', data=create_userdata_serial)
-    print(request)
-    print(request.json())
+        create_userdata_serial = json.dumps(create_userdata)
+
+        response = requests.put(f'https://teste-estudos-4706a-default-rtdb.firebaseio.com/user-data/{user_id}.json', data=create_userdata_serial)
+
+        if response.status_code == 200:
+            print(f"Cadastro realizado com sucesso! ID local gerado: {user_id}")
+        else:
+            print(f'Erro ao cadastrar: código de status {response.status_code}')
+
+        continuar = input("Deseja continuar cadastrando? (s/n): ").strip().lower()
+        if continuar != 's':
+            break
+
+def check_by_id():
+    while True:
+        id_a_procurar = str(input("Digite o ID que deseja procurar (ou 'q' para sair): ").strip().lower())
+        
+        if id_a_procurar == 'q':
+            break
+        
+        url = f'https://teste-estudos-4706a-default-rtdb.firebaseio.com/user-data/{id_a_procurar}.json'
+        response = requests.get(url, verify=False)  # Desativa a verificação do certificado SSL, sim, não é seguro, mas é para testes então tudo ok
+
+        if response.status_code == 200:
+            data = response.json()
+            if data is not None:
+                name = data.get('name')
+                if name:
+                    print(f"Nome encontrado para o ID '{id_a_procurar}': {name}")
+                else:
+                    print(f"Nome não encontrado para o ID '{id_a_procurar}'")
+            else:
+                print(f"ID '{id_a_procurar}' não encontrado no banco de dados")
+        else:
+            print(f'A solicitação falhou com o código de status {response.status_code}')
 
 cadastro(id_generator)
 
-lista = json.load()
-
-
+check_by_id()
